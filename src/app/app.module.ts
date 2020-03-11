ad1e4371cf10936be7cd30875a7ce2a46b6f9899
hatr1ck <38593812+hatr1ck@users.noreply.github.com>
2020-03-11 14:25:12 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
+import { ProductList } from './services/productList.service';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [ProductList],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
